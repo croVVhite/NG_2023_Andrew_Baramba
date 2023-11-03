6a5e4d51b047b267debaeb6bf40a0d4a2cc16ccb
Andrew Baramba <c.crovvhite.w@gmail.com>
2023-11-03 15:00:48 +0200

    Changed variables names in task 2

--- Lection1/task2.py
+++ Lection1/task2.py
@@ -1,2 +1,2 @@
-a,b = int(input("Enter first number for the sum: ")),int(input("\nEnter second number for the sum: "))
-print(a + b)
+firstNumber,secondNumber = int(input("Enter first number for the sum: ")),int(input("\nEnter second number for the sum: "))
+print(firstNumber + secondNumber)
