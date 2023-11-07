8c4bc192cad352e81598f8d4883c0531f9f1376b
Andrew Baramba <c.crovvhite.w@gmail.com>
2023-11-07 19:55:30 +0200

    Added second task for second lecture

---
+++ Lection2/task2.py
@@ -0,0 +1,4 @@
+userArray, digitAmount = [x for x in input("Input any elements in array via coma: ").split(',')], 0
+for element in userArray:
+    if element.isdigit(): digitAmount += 1
+print("There is " + str(digitAmount) + " digits in your array")
