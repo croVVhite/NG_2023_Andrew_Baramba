48993a0d05140c98e10e15fbf4e146865a2d6184
Andrew Baramba <c.crovvhite.w@gmail.com>
2023-11-19 22:45:37 +0200

    Removed unnecessary list

--- Lection2/task4.py
+++ Lection2/task4.py
@@ -1,5 +1,4 @@
 userText = [x for x in input("Input some text: ").lower()]
-vowels = ["a", "e", "i", "o", "u", "y", " "]
 
 for letter in userText:
-    if letter in vowels: print(letter, end="")
+    if letter in ["a", "e", "i", "o", "u", "y", " "]: print(letter, end="")
